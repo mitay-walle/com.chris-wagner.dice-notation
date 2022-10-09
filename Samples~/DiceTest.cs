e815f0317bf742a8a411a0827c7288ea2688549a
Dmitry <mitay-walle91@mail.ru>
2022-10-09 18:34:09 +0300

    samples fixes

--- Samples~/DiceTest.cs
+++ Samples~/DiceTest.cs
@@ -3,22 +3,26 @@ using UnityEngine;
 
 namespace mitaywalle.DiceNotation.Examples
 {
+    [ExecuteAlways]
     public class DiceTest : MonoBehaviour
     {
         private string _diceText = "3d6+4";
         private string _result;
-        
+
         void OnGUI()
         {
+            GUILayout.BeginArea(new Rect(50,50,400,1000));
             GUILayout.Label("Expression:");
             _diceText = GUILayout.TextArea(_diceText);
-            if (GUILayout.Button("Roll parsed expression"))
-            {
-                _result = Dice.Parse(_diceText).Roll().Value.ToString();
-            }
-            
-            GUILayout.Label($"Result: {_result}");
-        }
 
+            if (GUILayout.Button("Roll"))
+            {
+                DiceResult diceResult = Dice.Parse(_diceText).Roll();
+                _result = diceResult.Value.ToString();
+            }
+
+            GUILayout.Label($"Result: {_result}");
+            GUILayout.EndArea();
+        }
     }
 }
